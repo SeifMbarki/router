695a31ded04c8982d02ae7b1d9eb0157c77f4ae9
SeifMbarki <you@example.com>
2021-02-19 03:51:12 +0100

    first commit

---
+++ src/Component/MovieList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+import { Link } from "react-router-dom";
+const MovieList = ({ moviesData, title, rate, location, history }) => {
+  if (location.state) {
+    return (
+      <div>
+        <MovieCard el={location.state.el} />;{" "}
+        <button
+          style={{
+            color: "white",
+            backgroundColor: "Highlight",
+            marginTop: "2%",
+          }}
+          onClick={() => history.goBack()}
+        >
+          Go Back
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {moviesData
+        .filter((el) => el.name.toLowerCase().includes(title.toLowerCase()))
+        .filter((el) => el.rating >= rate)
+        .map((el) => (
+          <Link to={{ pathname: `/details/${el.name}`, state: { el: el } }}>
+            <MovieCard el={el} key={el.id} />
+          </Link>
+        ))}
+    </div>
+  );
+};
+
+export default MovieList;
